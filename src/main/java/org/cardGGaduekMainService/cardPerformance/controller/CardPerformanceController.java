a1828fc6ad4967cbf35439f710b592e4c2200b44
cseongbo <cseongbo17@gmail.com>
2025-08-04 17:19:12 +0900

    ♻️refractor: 토큰에서 내정보 가져오기 기능 추가 #86

--- src/main/java/org/cardGGaduekMainService/cardPerformance/controller/CardPerformanceController.java
+++ src/main/java/org/cardGGaduekMainService/cardPerformance/controller/CardPerformanceController.java
@@ -1,11 +1,14 @@
 package org.cardGGaduekMainService.cardPerformance.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.cardGGaduekMainService.auth.dto.LoginMember;
 import org.cardGGaduekMainService.cardPerformance.dto.CardPerformanceDTO;
 import org.cardGGaduekMainService.cardPerformance.service.CardPerformanceService;
 import org.cardGGaduekMainService.response.ApiResponse;
 import org.cardGGaduekMainService.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +22,12 @@ public class CardPerformanceController {
     private final CardPerformanceService cardPerformanceService;
 
 
-    @GetMapping("/api/members/{memberId}/cardPerformance")
+    @GetMapping("/api/members/cardPerformance")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<ApiResponse<List<CardPerformanceDTO>>> getCardPerformances(
-            @PathVariable("memberId") Long memberId) {
+            @AuthenticationPrincipal LoginMember loginMember) {
+        System.out.println(loginMember);
+        Long memberId = loginMember.getId();
         List<CardPerformanceDTO> cardPerformances = cardPerformanceService.getCardPerformances(memberId);
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.CARD_PERFORMANCE_FETCH_SUCCESS, cardPerformances));
     }
--- src/main/java/org/cardGGaduekMainService/cardSummary/controller/CardSummaryController.java
+++ src/main/java/org/cardGGaduekMainService/cardSummary/controller/CardSummaryController.java
@@ -1,11 +1,13 @@
 package org.cardGGaduekMainService.cardSummary.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.cardGGaduekMainService.auth.dto.LoginMember;
 import org.cardGGaduekMainService.cardSummary.dto.CardSummaryDTO;
 import org.cardGGaduekMainService.cardSummary.service.CardSummaryService;
 import org.cardGGaduekMainService.response.ApiResponse;
 import org.cardGGaduekMainService.response.SuccessCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,9 @@ public class CardSummaryController {
 
     private final CardSummaryService cardSummaryService;
 
-    @GetMapping("/api/members/{memberId}/summary")
-    public ResponseEntity<ApiResponse<List<CardSummaryDTO>>> getCardSummary(@PathVariable Long memberId) {
+    @GetMapping("/api/members/summary")
+    public ResponseEntity<ApiResponse<List<CardSummaryDTO>>> getCardSummary(@AuthenticationPrincipal LoginMember loginMember) {
+        Long memberId = loginMember.getId();
 
         List<CardSummaryDTO> cardSummaries = cardSummaryService.getMonthlyCardSummary(memberId);
         return ResponseEntity.ok(ApiResponse.success(SuccessCode.CARD_SUMMARY_FETCH_SUCCESS, cardSummaries));
